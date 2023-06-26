47d07d33e8cc7bfa0bc086d10042281f333e5d8a
ArnabBanik-repo <arnabbanik9@gmail.com>
2023-06-26 20:56:16 +0530

    changes in .gitignore

---
+++ backend/routes/user.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const {
+  getUsers,
+  updateMe,
+  deleteMe,
+  getMe,
+  getUser,
+  uploadUserPhoto,
+  resizePhoto,
+} = require('../controllers/user');
+const {
+  register,
+  login,
+  protect,
+  restrictTo,
+  forgotPassword,
+  resetPassword,
+  updatePassword,
+  logout,
+} = require('../controllers/auth');
+
+const router = express.Router();
+
+router.route('/register').post(register);
+router.route('/login').post(login);
+router.route('/logout').get(logout);
+
+router.route('/forgotPassword').post(forgotPassword);
+router.route('/resetPassword/:token').patch(resetPassword);
+
+router.use(protect);
+
+router.route('/updatePassword').patch(updatePassword);
+router.route('/me').get(getMe, getUser);
+router.route('/updateMe').patch(uploadUserPhoto, resizePhoto, updateMe);
+router.route('/deleteMe').delete(deleteMe);
+
+router.use(restrictTo('employee', 'admin'));
+
+router.route('/').get(getUsers);
+router.route('/:id').get(getUser);
+
+module.exports = router;
