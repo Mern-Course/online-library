733b621fda1a0d338922bb5824d6bc77bd195a58
Ekansh Agrawal <83202027+EkanshAgrawal@users.noreply.github.com>
2023-07-06 20:19:50 +0530

    book controller updated

--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -32,8 +32,34 @@ const issueBook = catchAsync(async (req, res, next) => {
 })
 
 
-const returnBook = catchAsync(async(req, res, next) => {
-  
+const returnBook = catchAsync(async (req, res, next) => {
+  const book = await Book.findById(req.params.id);
+  if (!book)
+    return next(new AppError("No such book exists"));
+
+  if (book.available === true)
+    return next(new AppError("Book is already returned"));
+
+  if (book.issuedBy.toString() !== req.user.id)
+    return next(new AppError("You are not authorized to return this book"));
+
+  const returnDate = new Date();
+  const diffInDays = Math.floor((returnDate - book.issuedOn) / (1000 * 60 * 60 * 24));
+  const fine = diffInDays > 14 ? (diffInDays - 14) * 10 : 0;
+
+  const updatedBook = await Book.findByIdAndUpdate(req.params.id, {
+    available: true,
+    issuedBy: null,
+    issuedOn: null,
+  }, { new: true });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      book: updatedBook,
+      fine: fine,
+    },
+  });
 })
 
 module.exports = {
