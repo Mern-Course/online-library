e45d03e2479de89b0265e2cafeae94a97860ee67
ArnabBanik-repo <arnabbanik9@gmail.com>
2023-07-07 13:04:16 +0530

    Book issue and return fixed

---
+++ backend/README.md
@@ -0,0 +1,65 @@
+# Library API
+
+---
+
+This is a backend API for an online library management platform. The API
+provides features to manage users and books. Users can register themselves into
+the library system, view books, issue books for themselves, and return books.
+All the basic functionalities of a library system have been provided.
+
+## Routes
+
+---
+
+### User
+
+1. /api/v1/users/register (POST) - Register new users into the system
+
+{ "name": "{{name}}", "email": "{{email}}", "password": "{{password}}",
+"confirmPassword":"{{password}}", "gender": "{{gender}}" }
+
+2. /api/v1/users/me (GET) - Get current user information
+3. /api/v1/users/<user_id> (GET) - Get a specific user information (for admins
+   only)
+4. /api/v1/users/logout (GET) - Log out
+5. /api/v1/users/login (POST) - Log in with email and password
+
+{ "email": "{{email}}", "password": "{{password}}" }
+
+6. /api/v1/users/forgotPassword (POST) - To reset password
+
+{ "email": "{{email}}" }
+
+7. /api/v1/users/resetPassword/<reset_token> (Information sent through mail) -
+   Reset password
+
+{ "password": "{{password}}", "confirmPassword": "{{password}}" }
+
+8. /api/v1/users/updateMe (PATCH) - Update user profile
+
+(FORM-DATA) email: {{email}} photo: {{photo file}}
+
+9. /api/v1/users/updatePassword (PATCH) - Update user password
+
+{ "password": "{{password}}", "newPassword": "super-strong-password",
+"confirmNewPassword": "super-strong-password" }
+
+10. /api/v1/users/deleteMe (DELETE) - Delete user account
+11. /api/v1/users (GET) - See all user information (for admins only)
+
+### Books
+
+1. /api/v1/books (GET) - See all book information
+2. /api/v1/books/<book_id> (GET) - See specific book information
+3. /api/v1/books (POST) - Add new books to the library (For admins only)
+
+{ "name": "{{name}}", "author": "{{author}}", "genre": ["{{genre1}}",
+"{{genre2}}", "{{genre3}}", ...], "ISBN": "{{ISBN}}", "rating": {{rating}} }
+
+4. /api/v1/books/<book_id> (PATCH) - Update book information (For admins only)
+
+{ "author": "{{New author name}}", "rating": {{new_rating}} }
+
+5. /api/v1/books/<book_id> (DELETE) - Delete book (For admins only)
+6. /api/v1/books/issue/<book_id> (GET) - Issue book
+7. /api/v1/books/return/<book_id> (GET) - Return book
--- backend/controllers/book.js
+++ backend/controllers/book.js
@@ -1,7 +1,8 @@
-const catchAsync = require('../utils/catchAsync');
-const Book = require('../models/book');
-const AppError = require('../utils/AppError');
-const { getOne, addOne, getAll, updateOne, deleteOne } = require('./factory');
+const catchAsync = require("../utils/catchAsync");
+const Book = require("../models/book");
+const User = require("../models/user");
+const AppError = require("../utils/AppError");
+const { getOne, addOne, getAll, updateOne, deleteOne } = require("./factory");
 
 const getBooks = getAll(Book);
 const createBook = addOne(Book);
@@ -11,40 +12,56 @@ const deleteBook = deleteOne(Book);
 
 const issueBook = catchAsync(async (req, res, next) => {
   const book = await Book.findById(req.params.id);
-  if(!book)
+  if (!book) {
     return next(new AppError("No such book exists"));
+  }
 
-  if(book.available === false)
+  if (book.available === false) {
     return next(new AppError("Book isn't available at the moment"));
+  }
 
   let updatedBook = {
     available: false,
     issuedBy: req.user.id,
     issuedOn: new Date(),
-  }
-  
+  };
+
   await Book.findByIdAndUpdate(book.id, updatedBook);
 
+  const updatedUser = {
+    booksIssued: [...(req.user.booksIssued), book._id],
+  };
+
+  await User.findByIdAndUpdate(req.user._id, updatedUser);
+  req.user = {
+    ...(req.user),
+    ...updatedUser,
+  };
+
   res.status(200).json({
-    status: 'success',
+    status: "success",
     data: updatedBook,
   });
-})
-
+});
 
 const returnBook = catchAsync(async (req, res, next) => {
   const book = await Book.findById(req.params.id);
-  if (!book)
+  if (!book) {
     return next(new AppError("No such book exists"));
+  }
 
-  if (book.available === true)
+  if (book.available === true) {
     return next(new AppError("Book is already returned"));
+  }
 
-  if (book.issuedBy.toString() !== req.user.id)
+  if (book.issuedBy.toString() !== req.user.id) {
     return next(new AppError("You are not authorized to return this book"));
+  }
 
   const returnDate = new Date();
-  const diffInDays = Math.floor((returnDate - book.issuedOn) / (1000 * 60 * 60 * 24));
+  const diffInDays = Math.floor(
+    (returnDate - book.issuedOn) / (1000 * 60 * 60 * 24),
+  );
   const fine = diffInDays > 14 ? (diffInDays - 14) * 10 : 0;
 
   const updatedBook = await Book.findByIdAndUpdate(req.params.id, {
@@ -54,14 +71,29 @@ const returnBook = catchAsync(async (req, res, next) => {
     returnDate,
   }, { new: true, runValidators: true });
 
+  let books = req.user.booksIssued;
+  const idx = books.indexOf(book._id);
+  books.splice(idx, 1);
+
+  const updatedUser = {
+    booksIssued: books,
+    fine,
+  };
+
+  await User.findByIdAndUpdate(req.user._id, updatedUser);
+  req.user = {
+    ...(req.user),
+    ...updatedUser,
+  };
+
   res.status(200).json({
-    status: 'success',
+    status: "success",
     data: {
       book: updatedBook,
-      fine, 
+      fine,
     },
   });
-})
+});
 
 module.exports = {
   getBooks,
@@ -70,5 +102,5 @@ module.exports = {
   updateBook,
   deleteBook,
   issueBook,
-  returnBook
+  returnBook,
 };
